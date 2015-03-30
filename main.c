42f4da08f13fdcdf16cc57439d752a5e7c488788
Andy <AndrewJasonKee@gmail.com>
2015-03-30 15:46:03 -0600

    started the main funciton

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+	Cache Simulation Project -- ECEN 4593
+	Authors: 
+		Andrew Kee
+		Ryan Montoya
+*/
+
+int main(int argc, char *argv[]){
+	parse_config(".config");
+}
+
+void parse_config(char* fileName){
+	
+}
+
+void report(){
+
+}
+
