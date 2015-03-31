b1a1770b1cf368050229741975818dd6fb8980b8
Andy <AndrewJasonKee@gmail.com>
2015-03-30 20:07:11 -0600

    made a makefile, and changed up main a bit

---
+++ Makefile
@@ -0,0 +1,5 @@
+main.o : main.c
+	gcc main.c -o main
+
+clean:
+	rm main.o 
--- main.c
+++ main.c
@@ -5,15 +5,16 @@
 		Ryan Montoya
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <String.h>
+#include "cache.h"
+
+static struct cache l1_data, l1_inst, l2, main_mem;
+
 int main(int argc, char *argv[]){
-	parse_config(".config");
+	parse_config("config.xml");
 }
 
-void parse_config(char* fileName){
-	
-}
 
-void report(){
-
-}
 
