ada3becd63272cdd4fcc1f5d3fc1bdddb410022c
Montoyaman <rymo8067@colorado.edu>
2015-04-08 21:33:56 -0600

    I'm pulling the ifdef stuff

--- LRU.c
+++ LRU.c
@@ -81,12 +81,16 @@ node* LRU_Update(cache* cache_level, uint set, uint block){
 	//Link the head to i_ptr
 	cache_level->cache_set[set].lru->head = i_ptr;
 	
+	printf("asdf");
+
 	return i_ptr;
 }
 
 unsigned int LRU_Get_LRU(cache* cache_level, uint set)
 {
 	struct node* cur_ptr = cache_level->cache_set[set].lru->head;
+
+	printf("asdf");
 
 	//Stuck in an infinite loop here, why?
 	while (cur_ptr->next)
