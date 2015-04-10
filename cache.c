4cda76fbe2b4665b8d0b2da0d69d96f7d41d3ba4
Andy <AndrewJasonKee@gmail.com>
2015-04-09 22:26:07 -0600

    pull

--- cache.c
+++ cache.c
@@ -209,15 +209,18 @@ void read_trace(cache* l1_data, cache* l1_inst, ull* num_inst, ull* num_reads, u
 	// printf("%lu\n", sizeof(unsigned long long int));
 	uint bytesize = 0;
 	while(scanf("%c %llx %d\n", &op, &address, &bytesize) == 3){
-		// printf("%c %llx %d\n", op, address, bytesize);
+		printf("%c %llx %d\n", op, address, bytesize);
 		if(op == 'I'){
 			*num_inst = *num_inst + 1;
+			look_through_cache(l1_inst, address, op);
 		} else if (op == 'R'){
 			*num_reads = *num_reads + 1;
+			look_through_cache(l1_data, address, op);
 		} else if (op == 'W'){
 			*num_writes = *num_writes + 1;
+			look_through_cache(l1_data, address, op);
 		}
-		look_through_cache(l1_inst, address, op);
+		
 		if((*num_inst + *num_reads + *num_writes) % 380000 == 0){
 			//write all dirty blocks to the next level of cache.  do this all the way down to main memory
 		}
@@ -296,49 +299,49 @@ void fetch_from_next_cache(cache* next_level, ulli tag, ulli index, uint assoc_l
 
 void report(cache* l1_data, cache* l1_inst, cache* l2, cache* main_mem, ull* num_inst, ull* num_reads, ull* num_writes){
 
-	// FILE * outputFile;
+	FILE * outputFile;
 
-	// outputFile = fopen("results.dat", "w+");
+	outputFile = fopen("results.dat", "w+");
 
-	// //Calculate the l1_inst things
-	// l1_inst->total_requests = l1_inst->num_hits + l1_inst->num_misses;
- //    l1_inst->hit_rate 	=	(double) l1_inst->num_hits 		/ 	l1_inst->total_requests 	* 100;
- //    l1_inst->miss_rate 	= 	(double) l1_inst->num_misses 	/ 	l1_inst->total_requests 	* 100;
+	//Calculate the l1_inst things
+	l1_inst->total_requests = l1_inst->num_hits + l1_inst->num_misses;
+    l1_inst->hit_rate 	=	(double) l1_inst->num_hits 		/ 	l1_inst->total_requests 	* 100;
+    l1_inst->miss_rate 	= 	(double) l1_inst->num_misses 	/ 	l1_inst->total_requests 	* 100;
 
- //    //Calculate the l1_data things
- //    l1_data->total_requests = l1_data->num_hits + l1_data->num_misses;
- //    l1_data->hit_rate 	=	(double) l1_data->num_hits 		/ 	l1_data->total_requests 	* 100;
- //    l1_data->miss_rate 	= 	(double) l1_data->num_misses 	/ 	l1_data->total_requests 	* 100;
+    //Calculate the l1_data things
+    l1_data->total_requests = l1_data->num_hits + l1_data->num_misses;
+    l1_data->hit_rate 	=	(double) l1_data->num_hits 		/ 	l1_data->total_requests 	* 100;
+    l1_data->miss_rate 	= 	(double) l1_data->num_misses 	/ 	l1_data->total_requests 	* 100;
 
- //    //Calculate the l2 things
- //    l2->total_requests = l2->num_hits + l2->num_misses;
- //    l2->hit_rate 		=	(double) l2->num_hits 			/ 	l2->total_requests 		* 100;
- //    l2->miss_rate 		= 	(double) l2->num_misses 		/ 	l2->total_requests 		* 100;
+    //Calculate the l2 things
+    l2->total_requests = l2->num_hits + l2->num_misses;
+    l2->hit_rate 		=	(double) l2->num_hits 			/ 	l2->total_requests 		* 100;
+    l2->miss_rate 		= 	(double) l2->num_misses 		/ 	l2->total_requests 		* 100;
 
- //    //Calculate percent of reference types
- //    ull total_traces 		= *num_inst + *num_reads + *num_writes;
- //    double inst_percent 	= *num_inst 	/ total_traces 	* 100;
- //    double read_precent 	= *num_reads 	/ total_traces 	* 100;
- //    double write_percent 	= *num_writes 	/ total_traces 	* 100;
+    //Calculate percent of reference types
+    ull total_traces 		= *num_inst + *num_reads + *num_writes;
+    double inst_percent 	= *num_inst 	/ total_traces 	* 100;
+    double read_precent 	= *num_reads 	/ total_traces 	* 100;
+    double write_percent 	= *num_writes 	/ total_traces 	* 100;
 
-	// fprintf(outputFile, "Memory System: \n");
-	// fprintf(outputFile, "	Dcache size = %u  :  ways = %u  :  block size = %u \n", l1_data->cache_size, l1_data->assoc, l1_data->block_size);
-	// fprintf(outputFile, "	Icache size = %u  :  ways = %u  :  block size = %u \n", l1_inst->cache_size, l1_inst->assoc, l1_inst->block_size);
-	// fprintf(outputFile, "	L2-cache size = %u  :  ways = %u  :  block size = %u \n", l2->cache_size, l2->assoc, l2->block_size);
-	// fprintf(outputFile, "	Memory ready time = %u  :  chunksize = %u  :  chunktime = %u \n", main_mem->mem_ready, main_mem->mem_chunksize, main_mem->mem_chunktime);
-	// fprintf(outputFile, "\n");
+	fprintf(outputFile, "Memory System: \n");
+	fprintf(outputFile, "	Dcache size = %u  :  ways = %u  :  block size = %u \n", l1_data->cache_size, l1_data->assoc, l1_data->block_size);
+	fprintf(outputFile, "	Icache size = %u  :  ways = %u  :  block size = %u \n", l1_inst->cache_size, l1_inst->assoc, l1_inst->block_size);
+	fprintf(outputFile, "	L2-cache size = %u  :  ways = %u  :  block size = %u \n", l2->cache_size, l2->assoc, l2->block_size);
+	fprintf(outputFile, "	Memory ready time = %u  :  chunksize = %u  :  chunktime = %u \n", main_mem->mem_ready, main_mem->mem_chunksize, main_mem->mem_chunktime);
+	fprintf(outputFile, "\n");
 
 	// fprintf(outputFile, "Execute time 	=	%llu;		Total refs	= %llu\n", , l1_inst->total_requests + l1_data->total_requests);
 	// fprintf(outputFile, "Flush time 		=	%llu", );
 	// fprintf(outputFile, "Inst refs 		=	%llu; 	Data refs 	= %llu", l1_inst->total_requests, l1_data->total_requests);
 	// fprintf(outputFile, "\n");
 
-	// fprintf(outputFile, "Number of reference types : 	[Percentage]\n");
-	// fprintf(outputFile, "	Reads 	= 	%llu 			[%.1f%%]\n", *num_reads, 	read_precent);
-	// fprintf(outputFile, "	Writes 	= 	%llu 			[%.1f%%]\n", *num_writes, 	write_percent);
-	// fprintf(outputFile, "	Inst. 	= 	%llu			[%.1f%%]\n", *num_inst, 	inst_percent);
-	// fprintf(outputFile, "	Total 	= 	%llu\n ", total_traces);
-	// fprintf(outputFile, "\n");
+	fprintf(outputFile, "Number of reference types : 	[Percentage]\n");
+	fprintf(outputFile, "	Reads 	= 	%llu 			[%.1f%%]\n", *num_reads, 	read_precent);
+	fprintf(outputFile, "	Writes 	= 	%llu 			[%.1f%%]\n", *num_writes, 	write_percent);
+	fprintf(outputFile, "	Inst. 	= 	%llu			[%.1f%%]\n", *num_inst, 	inst_percent);
+	fprintf(outputFile, "	Total 	= 	%llu\n ", total_traces);
+	fprintf(outputFile, "\n");
 
 	// fprintf(outputFile, "Total cycles for activities: 	[Percentage]\n");
 	// fprintf(outputFile, "	Reads 	= 	%llu 			[%.1f%%]\n", , );
@@ -353,33 +356,33 @@ void report(cache* l1_data, cache* l1_inst, cache* l2, cache* main_mem, ull* num
 	// fprintf(outputFile, "Ideal mis-aligned: Exec. Time = %llu; CPI = %d\n", , );
 	// fprintf(outputFile, "\n");
 
-	// fprintf(outputFile, "Memory Level: 	L1i");
-	// fprintf(outputFile, "	Hit Count = %llu 		Miss Count = %llu\n", l1_inst->num_hits, l1_inst->num_misses);
-	// fprintf(outputFile, "	Total Requests = %llu", l1_inst->total_requests);
-	// fprintf(outputFile, " 	Hit Rate = %.1f%% 	Miss Rate = %.1f%%\n", l1_inst->hit_rate, l1_inst->miss_rate);
-	// fprintf(outputFile, "	Kickouts = %llu; Dirty Kickouts = %llu; Transfers = %llu\n", l1_inst->kickouts, l1_inst->dirty_kickouts, l1_inst->transfers);
-	// fprintf(outputFile, "Flush Kickouts = %llu\n", l1_inst->flush_kickouts);
-	// fprintf(outputFile, "\n");
+	fprintf(outputFile, "Memory Level: 	L1i\n");
+	fprintf(outputFile, "	Hit Count = %llu 		Miss Count = %llu\n", l1_inst->num_hits, l1_inst->num_misses);
+	fprintf(outputFile, "	Total Requests = %llu", l1_inst->total_requests);
+	fprintf(outputFile, " 	Hit Rate = %.1f%% 	Miss Rate = %.1f%%\n", l1_inst->hit_rate, l1_inst->miss_rate);
+	fprintf(outputFile, "	Kickouts = %llu; Dirty Kickouts = %llu; Transfers = %llu\n", l1_inst->kickouts, l1_inst->dirty_kickouts, l1_inst->transfers);
+	fprintf(outputFile, "Flush Kickouts = %llu\n", l1_inst->flush_kickouts);
+	fprintf(outputFile, "\n");
 
-	// fprintf(outputFile, "Memory Level: 	L1d");
-	// fprintf(outputFile, "	Hit Count = %llu 		Miss Count = %llu\n", l1_data->num_hits, l1_data->num_misses);
-	// fprintf(outputFile, "	Total Requests = %llu", l1_data->total_requests);
-	// fprintf(outputFile, " 	Hit Rate = %.1f%% 	Miss Rate = %.1f%%\n", l1_data->hit_rate, l1_data->miss_rate);
-	// fprintf(outputFile, "	Kickouts = %llu; Dirty Kickouts = %llu; Transfers = %llu\n", l1_data->kickouts, l1_data->dirty_kickouts, l1_data->transfers);
-	// fprintf(outputFile, "Flush Kickouts = %llu\n", l1_data->flush_kickouts);
-	// fprintf(outputFile, "\n");
+	fprintf(outputFile, "Memory Level: 	L1d\n");
+	fprintf(outputFile, "	Hit Count = %llu 		Miss Count = %llu\n", l1_data->num_hits, l1_data->num_misses);
+	fprintf(outputFile, "	Total Requests = %llu", l1_data->total_requests);
+	fprintf(outputFile, " 	Hit Rate = %.1f%% 	Miss Rate = %.1f%%\n", l1_data->hit_rate, l1_data->miss_rate);
+	fprintf(outputFile, "	Kickouts = %llu; Dirty Kickouts = %llu; Transfers = %llu\n", l1_data->kickouts, l1_data->dirty_kickouts, l1_data->transfers);
+	fprintf(outputFile, "Flush Kickouts = %llu\n", l1_data->flush_kickouts);
+	fprintf(outputFile, "\n");
 
-	// fprintf(outputFile, "Memory Level: 	L2");
-	// fprintf(outputFile, "	Hit Count = %llu 		Miss Count = %llu\n", l2->num_hits, l2->num_misses);
-	// fprintf(outputFile, "	Total Requests = %llu", l2->total_requests);
-	// fprintf(outputFile, " 	Hit Rate = %.1f%% 	Miss Rate = %.1f%%\n", l2->hit_rate, l2->miss_rate);
-	// fprintf(outputFile, "	Kickouts = %llu; Dirty Kickouts = %llu; Transfers = %llu\n", l2->kickouts, l2->dirty_kickouts, l2->transfers);
-	// fprintf(outputFile, "Flush Kickouts = %llu\n", l2->flush_kickouts);
-	// fprintf(outputFile, "\n");
+	fprintf(outputFile, "Memory Level: 	L2\n");
+	fprintf(outputFile, "	Hit Count = %llu 		Miss Count = %llu\n", l2->num_hits, l2->num_misses);
+	fprintf(outputFile, "	Total Requests = %llu", l2->total_requests);
+	fprintf(outputFile, " 	Hit Rate = %.1f%% 	Miss Rate = %.1f%%\n", l2->hit_rate, l2->miss_rate);
+	fprintf(outputFile, "	Kickouts = %llu; Dirty Kickouts = %llu; Transfers = %llu\n", l2->kickouts, l2->dirty_kickouts, l2->transfers);
+	fprintf(outputFile, "Flush Kickouts = %llu\n", l2->flush_kickouts);
+	fprintf(outputFile, "\n");
 
 	// fprintf(outputFile, "L1 cache cost (Icache $%d) + (Dcache $%d) = $%d", , , );
 	// fprintf(outputFile, "L1 cache cost = $%d; Memory cost = $%d; Total cost = $%d", , , );
 	// fprintf(outputFile, "Flushes = %f  :  Invalidates = %f", , );
 
-	// fclose(outputFile);
+	fclose(outputFile);
 }
